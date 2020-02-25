725ca489a8815bd2efee488bc14a0ba5fc737959
Tanmoy_Chowdhury <tanmoy22chowdhury@gmail.com>
2020-02-25 16:51:28 +0530

    added cfig

--- src/main/java/com/serverlessspringdynamo/serverlessspringdynamo/Configuration/DynamoDBConfig.java
+++ src/main/java/com/serverlessspringdynamo/serverlessspringdynamo/Configuration/DynamoDBConfig.java
@@ -1,17 +1,16 @@
 package com.serverlessspringdynamo.serverlessspringdynamo.Configuration;
 
-import java.lang.annotation.Annotation;
-
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDB;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig.PaginationLoadingStrategy;
 
 @Configuration
 @EnableDynamoDBRepositories
@@ -19,9 +18,18 @@ public class DynamoDBConfig {
 
 	@Bean
 	public DynamoDBMapper dynamodbmapper() {
-		return new DynamoDBMapper(new AmazonDynamoDBClient
-				(AmazonDynamoDBClientBuilder.standard().withRegion("")
-						.withCredentials(new BasicAWSCredentials("", "")))
-				.build())))
+		return new DynamoDBMapper(dynamodbprovider(), config());
+	}
+	private DynamoDBMapperConfig config() {
+		return DynamoDBMapperConfig.builder().withPaginationLoadingStrategy(PaginationLoadingStrategy.LAZY_LOADING)
+				.build();
+	}
+	@Bean
+	public AmazonDynamoDB dynamodbprovider() {
+		return AmazonDynamoDBClientBuilder.standard().withCredentials(credentialsProvider()).build();
+	}
+	@Bean
+	private AWSCredentialsProvider credentialsProvider() {
+		return new AWSStaticCredentialsProvider(new BasicAWSCredentials("", ""));
 	}
 }
